ee288f2d2925b38a01ee722b3338b8990e721a7a
monir0153 <mohammadmonir0153@gmail.com>
2024-11-06 12:02:57 +0600

    fix macro function

--- app/Http/Controllers/Authenticate/AuthController.php
+++ app/Http/Controllers/Authenticate/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Authenticate;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +13,12 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create($request->validated());
+        $token = Auth::login($user);
+        return Response::success(['token' => $token, 'user' => $user], 'User registered successfully');
+    }
     public function login(LoginRequest $request)
     {
         $token = Auth::attempt($request->only('email', 'password'));
@@ -28,5 +36,9 @@ class AuthController extends Controller
     public function profile(Request $request)
     {
         return Response::success(['user' => $request->user()]);
+    }
+    public function refresh()
+    {
+        return Response::success(['token' => Auth::refresh()]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->controller(AuthController::class)->group(function () {
+    Route::post('register', 'register')->withoutMiddleware('auth:api');
     Route::post('login', 'login')->withoutMiddleware('auth:api');
     Route::post('logout', 'logout');
     Route::get('profile', 'profile');
+    Route::get('refresh', 'refresh');
 });
